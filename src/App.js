a696675573f574a0ebdce65f399a0b80903fa522
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-23 13:55:26 +0900

    Add header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Header } from "./components/header/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="app" >
+      <Header />
+    </div>
+  );
 }
 
 export default App;
